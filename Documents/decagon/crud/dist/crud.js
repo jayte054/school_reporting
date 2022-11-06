c01fb0c99bb392d2ddbdd3ce1ec8aa59423832fc
jayte054 <54476858+jayte054@users.noreply.github.com>
2022-11-06 21:37:48 +0100

    push

--- Documents/decagon/crud/dist/crud.js
+++ Documents/decagon/crud/dist/crud.js
@@ -4,12 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const author_1 = __importDefault(require("./routes/author"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.use(body_parser_1.default.json());
-app.use("/", author_1.default);
+app.use(express_1.default.json());
+const getUserData = () => {
+    const jsonData = require(path_1.default.join(__dirname, "database.json"));
+    console.log(jsonData);
+    return JSON.parse(jsonData);
+};
+const saveUserData = (data) => {
+    const stringifyData = JSON.stringify(data);
+    fs_1.default.writeFileSync("./database.json", stringifyData);
+};
+app.get("/", (req, res) => {
+    const output = getUserData();
+    res.send(output);
+});
+app.post("/user/data", (req, res) => {
+    const existUser = getUserData();
+    const userData = req.body;
+    existUser.push(userData);
+});
 app.listen(2000, () => {
-    console.log("the server is listening 2000");
+    console.log("app is listening on port 2000");
 });
 //# sourceMappingURL=crud.js.map
